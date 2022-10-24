92ced5b662decbc628fd1cdba24e54b83c37fe30
JuanDaReiR <114205848+JuanDaReiR@users.noreply.github.com>
2022-10-24 07:58:06 -0500

    VideoGameApp.java

---
+++ src/ui/VideoGameApp.java
@@ -0,0 +1,12 @@
+package ui;
+import model.VideoGame;
+
+public class VideoGameApp{
+        public static void main(String args[]) {
+        VideoGame vg = new VideoGame();
+
+        System.out.println("Press enter to continue");
+        vg.desplegarMenu();
+
+    }
+}
